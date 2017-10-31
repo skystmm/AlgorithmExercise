9912ab0da6009a89d3ec9a1847da9837137ca9d9
skystmm <st231@163.com>
2017-10-31 22:42:57 +0800

    ADD.添加35. Search Insert Position解题代码

---
+++ src/main/java/com/skystmm/array/SearchInsertPosition.java
@@ -0,0 +1,50 @@
+package com.skystmm.array;
+
+/**
+ * 35. Search Insert Position
+ * Created by Administrator on 2017/10/30.
+ */
+
+public class SearchInsertPosition {
+    /**
+     * AC time:5 ms beat:59.06%
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int solution(int[] nums, int target) {
+        int position = 0;
+        for(int i =0;i<nums.length ; i++){
+            if(target <= nums[i]){
+                return i;
+            }
+        }
+        return nums.length;
+    }
+
+    /**
+     * 二分查找
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int solutionV2(int[] nums, int target) {
+        if(nums == null) return 0;
+        return binarySearch(nums, 0, nums.length-1, target);
+    }
+
+    public int binarySearch(int[] nums, int left, int right, int target){
+
+        if(right >= left){
+            int mid = left + (right - left) / 2;
+            if(target == nums[mid]) return mid;
+            if(target < nums[mid]) return binarySearch(nums, left, mid - 1, target);
+            if(target > nums[mid]) return binarySearch(nums, mid + 1, right, target);
+        }
+
+        return left;
+
+    }
+
+
+}
