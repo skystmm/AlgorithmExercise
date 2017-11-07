6f50b56f7b01271ede8387b72c3a39866bb207dd
skystmm <st231@163.com>
2017-11-07 23:48:16 +0800

    ADD 581. Shortest Unsorted Continuous Subarray

---
+++ src/main/java/com/skystmm/array/ShortestUnsortedContinuousSubarray.java
@@ -0,0 +1,31 @@
+package com.skystmm.array;
+
+import javax.swing.*;
+import java.util.List;
+
+/**
+ * 581. Shortest Unsorted Continuous Subarray
+ * Created by Administrator on 2017/11/2.
+ */
+
+public class ShortestUnsortedContinuousSubarray {
+    /**
+     * UAC time :28 ms beat:52.41%
+     * @param nums
+     * @return
+     */
+    public static int solution(int[] nums) {
+        int n = nums.length, beg = -1, end = -2, min = nums[n-1], max =nums[0];
+        for (int i=1;i<n;i++) {
+            max = Math.max(max, nums[i]);
+            min = Math.min(min, nums[n-1-i]);
+            if (nums[i] < max) end = i;
+            if (nums[n-1-i] > min) beg = n-1-i;
+        }
+        return end - beg + 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ShortestUnsortedContinuousSubarray.solution(new int[]{1,3,2,2,2}));
+    }
+}
