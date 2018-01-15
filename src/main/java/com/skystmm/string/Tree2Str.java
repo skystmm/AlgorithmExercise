b4b3a7f350364e9fcfa29ec4e4bf0ffcc710a93c
skystmm <st231@163.com>
2018-01-15 23:09:02 +0800

    ADD.添加606. Construct String from Binary Tree解题代码

---
+++ src/main/java/com/skystmm/string/Tree2Str.java
@@ -0,0 +1,26 @@
+package com.skystmm.string;
+
+import com.skystmm.common.TreeNode;
+
+/**
+ * 606. Construct String from Binary Tree
+ * Created by Administrator on 2018/1/14.
+ */
+
+public class Tree2Str {
+    /**
+     * use recursion
+     * AC time:30 ms beats:64.07 %
+     * @param t
+     * @return
+     */
+    public String tree2str(TreeNode t) {
+        if (t == null)
+            return "";
+        if (t.left == null && t.right == null)
+            return t.val + "";
+        if (t.right == null)
+            return t.val + "(" + tree2str(t.left) + ")";
+        return t.val + "(" + tree2str(t.left) + ")(" + tree2str(t.right) + ")";
+    }
+}
