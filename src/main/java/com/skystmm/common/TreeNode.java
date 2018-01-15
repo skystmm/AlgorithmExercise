33cec1d3074e9a1d4bb4b6d8e4fa0ae741fec66d
skystmm <st231@163.com>
2018-01-15 23:10:52 +0800

    ADD.公共类TreeNode

---
+++ src/main/java/com/skystmm/common/TreeNode.java
@@ -0,0 +1,12 @@
+package com.skystmm.common;
+
+/**
+ * Created by Administrator on 2018/1/15.
+ */
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    TreeNode(int x) { val = x; }
+}
