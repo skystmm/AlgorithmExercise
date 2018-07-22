4c2d62e484015dff01b4f43f40dc4bbb0eb3374c
skystmm <st231@163.com>
2018-07-22 22:10:53 +0800

    ADD.添加832.Flipping an Image解题代码

---
+++ src/main/java/com/skystmm/array/FlippingAnImage.java
@@ -0,0 +1,48 @@
+package com.skystmm.array;
+
+import java.util.Arrays;
+
+/**
+ * 832.Flipping an Image
+ * Created by sky on 2018/7/22.
+ */
+public class FlippingAnImage {
+    /**
+     * AC use stream times: 48 ms
+     * @param A
+     * @return
+     */
+    public int[][] solution(int[][] A) {
+        Arrays.stream(A).forEach(item->{
+            int len = item.length;
+            int start = 0 ;
+            int end = len-1;
+            while(start <= end){
+                int tmp = item[start] ^ 1;
+                item[start] = item[end] ^1;
+                item[end] = tmp;
+                start++;
+                end--;
+            }
+        });
+        return A;
+    }
+
+    /**
+     * AC C-Style times: 5 ms
+     * @param A
+     * @return
+     */
+    public int[][] solution2(int[][] A){
+        int C = A[0].length;
+        for (int[] row: A)
+            for (int i = 0; i < (C + 1) / 2; ++i) {
+                int tmp = row[i] ^ 1;
+                row[i] = row[C - 1 - i] ^ 1;
+                row[C - 1 - i] = tmp;
+            }
+
+        return A;
+    }
+
+}
