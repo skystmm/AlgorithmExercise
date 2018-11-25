3650df8f3b5f34ac3ba4b5b14a835a6749176a1a
skystmm <st231@163.com>
2018-11-25 20:56:55 +0800

    ADD.添加566. Reshape the Matrix解题代码

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # leetSolute
-leetcode练习
+
+> leetcode 解题练习
+
+微目标：123/day（easy->1天，normal->2天,hard -> 3天）
+
+更新记录
+| 题目编号 | 名称 | 难度 | 提交时间 |
+| ------ | ------ | ------ |------ |
+| 566 | Reshape the Matrix | easy | 20181125 |
+| 766 | Toeplitz Matrix | easy | 20181125  |
+| 922 | Sort Array By Parity II | easy | 20181124 |
+| 867 | Transpose Matrix | easy | 20181123 |
+| 905 | Sort Array By Parity| easy | 20181122 |
+
+
---
+++ src/main/java/com/skystmm/array/ReshapeMatrix.java
@@ -0,0 +1,39 @@
+package com.skystmm.array;
+
+
+/**
+ * 566. Reshape the Matrix
+ * Created by Administrator on 2017/10/31.
+ */
+public class ReshapeMatrix {
+    /**
+     * time O(m*n) space: O(n*m)
+     *  AC  time :4ms,beats:95.09%
+     * @param nums
+     * @param r
+     * @param c
+     * @return
+     */
+    public int[][] solution(int[][] nums, int r, int c) {
+        int row = nums.length;
+        int column = nums[0].length;
+        if( r*c != row*column){
+            return  nums;
+        }
+        int[][] res = new int[r][c];
+        int rIndex= 0;
+        int cIndex = 0;
+        for(int i =0 ; i<row;i++){
+            for(int j = 0;j< column; j++){
+                res[rIndex][cIndex] = nums[i][j];
+                cIndex++;
+                if(cIndex == c){
+                    cIndex = 0;
+                    rIndex++;
+                }
+            }
+        }
+        return res;
+    }
+
+}
